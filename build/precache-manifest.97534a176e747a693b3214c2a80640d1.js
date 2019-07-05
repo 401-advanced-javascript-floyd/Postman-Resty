3d1341260080f4a62fb3f6e733da48ea78f746d5
Floyd Orr <floydcanhelp@gmail.com>
2019-07-05 17:27:47 -0500

    adding files

---
+++ build/precache-manifest.97534a176e747a693b3214c2a80640d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a6f6b6d453ba775f4c6c47392c5d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb4c146ca7729e8f913",
+    "url": "/static/css/main.c5f96315.chunk.css"
+  },
+  {
+    "revision": "a8c1c8c7370c4ac41264",
+    "url": "/static/js/2.d8e7af57.chunk.js"
+  },
+  {
+    "revision": "3cb4c146ca7729e8f913",
+    "url": "/static/js/main.952efc09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
